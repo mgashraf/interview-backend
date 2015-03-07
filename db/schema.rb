49038b643e142b8f0cf0f5509ae8c8b2c437db3b
Chubes <Chubes@andrew-mercers-macbook.local>
2015-03-07 00:41:52 -0500

    adding paperclip and resume model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218011657) do
+ActiveRecord::Schema.define(version: 20150307052750) do
+
+  create_table "resumes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
