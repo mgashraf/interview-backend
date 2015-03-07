938f0b75c0ff160efcd90c3cdbeb1dea0b386d13
Andrew Mercer <diddydodge27@yahoo.com>
2015-03-07 14:19:51 -0500

    resume posting.

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,15 @@
 class ResumesController < ApplicationController
   before_action :authenticate_user_from_token!
-  # def create
-  # end
+
+  def create
+    @resume = current_user.resumes.create(resume_params)
+    render json: {message: "It worked yo!"}, status: :created
+  end
+
+  private
+  def resume_params
+    params.require(:resume).permit(:name, :document)
+  end
 
   # def index
   # end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -2,7 +2,6 @@ class Resume < ActiveRecord::Base
   belongs_to :user
   has_many :listings
 
-  has_attached_file :document 
-
-  validates_attachment_content_type :pdf, { content_type: ['application/pdf'] }
+  has_attached_file :document   
+  validates_attachment_content_type :document, { content_type: ['application/pdf'] }
 end
